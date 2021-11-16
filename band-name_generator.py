391454717b6595cb526a9d4f9f3a996c9c1a0ec2
a-afful <49870112+a-afful@users.noreply.github.com>
2021-11-16 19:18:11 +0000

    Update and rename main.py to band-name_generator.py

--- band-name_generator.py
+++ band-name_generator.py
@@ -7,5 +7,3 @@ pet_name = input('What is the name of your pet?\n')
 #4. Combine the name of their city and pet and show them their band name.
 band_name = city + " " + pet_name
 print('Your band name should be: ' + band_name)
-#5. Make sure the input cursor shows on a new line, see the example at:
-#   https://band-name-generator-end.appbrewery.repl.run/
